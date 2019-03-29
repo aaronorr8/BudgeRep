677b86cfb852bf2216874d7fec2987c24940b5c1
aaronorr8 <aaronorr8@gmail.com>
2019-03-29 08:38:55 -0600

    Added Welcome screen

---
+++ BudgeAppMaster/SignUp/WelcomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  WelcomeViewController.swift
+//  BudgeAppMaster
+//
+//  Created by Aaron Orr on 3/29/19.
+//  Copyright © 2019 Icecream. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var getStartedButton: UIButton!
+    
+    
+    override func viewDidLayoutSubviews() {
+        
+        //Add rounded outline to save button
+        getStartedButton.backgroundColor = .clear
+        getStartedButton.layer.cornerRadius = 6
+        getStartedButton.layer.borderWidth = 2
+        getStartedButton.layer.borderColor = #colorLiteral(red: 0.2549019608, green: 0.4588235294, blue: 0.01960784314, alpha: 1)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+  
+
+}
